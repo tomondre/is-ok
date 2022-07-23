72daea1507003f69d3fe76c87300b200dd136aff
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-07-23 23:16:30 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import fetch from "node-fetch";
+import cors from 'cors'
+
+let app = express();
+app.use(cors());
+
+app.get('/', async (req, res) => {
+    let url = req.query.url;
+
+    if (url === undefined) {
+        res.status(400);
+        res.send('Please provide url parameter');
+    }
+
+    try {
+        res.send(await fetch(url));
+    } catch (error) {
+        res.status(404);
+        res.send(error);
+    }
+});
+
+app.get('/health', (req, res) => {
+    res.send('I am fine, let me sleep!');
+});
+
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
